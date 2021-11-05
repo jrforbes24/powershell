0e0fdad499f3bd6740d123b86e0e1088e00bc422
John R. Forbes <jrforbes24@gmail.com>
2021-11-05 10:16:28 -0600

    added two ps1 scripts

---
+++ get-creds.ps1
@@ -0,0 +1,8 @@
+﻿$userName = "johnrforbes@outlook.com"
+$password = Get-Content C:\Users\johnr\powershell\password.txt | ConvertTo-SecureString 
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $userName, $password
+Write-Host $cred.UserName
+
+
+Get-WmiObject Win32_DiskDrive -ComputerName "forbe5" 
+Get-Process | FT
--- helloWorld.ps1
+++ helloWorld.ps1
@@ -1,2 +1,18 @@
-﻿Clear
-Write-Host( " `n `nHello World! `n `n `n" )
+﻿
+function Say-Hello
+{    
+   Write-Host "The function is now executing." 
+   Write-Host( " `n `nHello World! `n `n" )
+   Write-Host( " `n `nHello World! `n `n" )
+   Write-Host( " `n `nHello World! `n `n" )
+   Write-Host( " `n `nHello World! `n `n" )
+   Write-Host( " `n `nHello World! `n `n" )
+
+} 
+
+
+Clear
+
+Write-Host "The main script body is now executing."
+
+Say-Hello
